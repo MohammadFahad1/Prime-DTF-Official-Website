7abf1c12fb7b0ed14642452e97cd7160af0589fd
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-10-27 20:53:15 +0600

    Customers review section partially done

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import CustomerReviews from "./components/CustomerReviews";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <CustomerReviews />
     </>
   );
 }
