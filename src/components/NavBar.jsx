cfe82b10a0294aef5b39ae6be00419d8ca90b817
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-10-29 19:42:33 +0600

    Navbar layout got fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,7 +24,7 @@ export default function NavBar() {
   );
 
   return (
-    <div className="navbar bg-[#19191C] text-white font-extrabold shadow-sm max-w-7xl mx-auto px-10">
+    <div className="navbar bg-[#19191C] text-white font-extrabold shadow-sm w-full px-10">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
