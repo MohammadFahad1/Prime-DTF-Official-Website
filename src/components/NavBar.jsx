4dfd71647210aeda9af258181df7958e333df12e
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-10-31 14:27:45 +0600

    Top Header section added to the project

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,43 +24,72 @@ export default function NavBar() {
   );
 
   return (
-    <nav className="navbar bg-white text-[#19191C] font-extrabold shadow-sm w-full px-10">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <>
+      {/* Top Header */}
+      <marquee
+        loop={true}
+        className="bg-[#bf2340] px-5 py-2 text-xl font-bold text-white m-0"
+      >
+        <span className="mr-8 my-0">
+          Working Hours: 10:30 AM to 8:30 PM | Working Days: Saturday - Thursday
+          | (B2B Only)
+        </span>
+        <span className="mr-8 my-0">
+          Working Hours: 10:30 AM to 8:30 PM | Working Days: Saturday - Thursday
+          | (B2B Only)
+        </span>
+        <span className="mr-8 my-0">
+          Working Hours: 10:30 AM to 8:30 PM | Working Days: Saturday - Thursday
+          | (B2B Only)
+        </span>
+      </marquee>
+      <div className="bg-black px-5 py-2 text-md text-center font-bold text-white my-0">
+        <p className="my-0">Acid Wash Drop is Back in Stock!</p>
+      </div>
+      {/* Navbar */}
+      <nav className="navbar bg-white text-[#19191C] font-extrabold shadow-sm w-full px-10">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-[#19191C] rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+              {NavMenu}
+            </ul>
           </div>
-          <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-[#19191C] rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            {NavMenu}
-          </ul>
+          <Link href={"/"}>
+            <Image
+              src={"/assets/logo.png"}
+              width={100}
+              height={80}
+              alt="logo"
+            />
+          </Link>
         </div>
-        <Link href={"/"}>
-          <Image src={"/assets/logo.png"} width={100} height={80} alt="logo" />
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 text-lg">{NavMenu}</ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Login</a>
-      </div>
-    </nav>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-lg">{NavMenu}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Login</a>
+        </div>
+      </nav>
+    </>
   );
 }
